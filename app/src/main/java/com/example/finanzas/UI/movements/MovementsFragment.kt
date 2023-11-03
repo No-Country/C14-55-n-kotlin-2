d4b5791812891f9629091f084adc2f44425646f8
ChristopherPeralta <cperaltapenaloza@gmail.com>
2023-11-03 10:40:04 -0500

    Movements_Fragment
    ReciclerView Horizontal

--- app/src/main/java/com/example/finanzas/UI/movements/MovementsFragment.kt
+++ app/src/main/java/com/example/finanzas/UI/movements/MovementsFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.finanzas.data.database.entities.MovementsEntity
 import com.example.finanzas.databinding.FragmentMovementsBinding
 
 
 class MovementsFragment : Fragment() {
+
     private var _binding: FragmentMovementsBinding? = null
     private val binding get() = _binding!!
 
@@ -16,10 +19,22 @@ class MovementsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentMovementsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val recyclerView = binding.rvFilterMovements
+        val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = layoutManager
+
+        // Aquí puedes configurar el adaptador del RecyclerView y otros detalles según tus necesidades.
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
